6a62b9ecea9b2da05f74c682fa5ea782a38ffec4
Greg DeLozier <gregdelozier@gmail.com>
2017-07-25 20:38:08 -0400

    Google line charts template

--- code/webapp/app.py
+++ code/webapp/app.py
@@ -13,6 +13,8 @@ def get_example():
    ]
    return template('make_table.tpl', rows=rows)
 
+# example page: https://developers.google.com/chart/interactive/docs/quick_start
+
 @route('/chart')
 def get_chart():
     pizzas = [
@@ -20,10 +22,60 @@ def get_chart():
           {'type':'Onions', 'count':1},
           {'type':'Olives', 'count':1},
           {'type':'Zucchini', 'count':5},
-          {'type':'Pepperoni', 'count':2}
+          {'type':'Pepperoni', 'count':2},
+          {'type':"Cheese",'count':10}
     ]
     return template("example_chart.tpl", rows=pizzas)
     #return template('make_dict_table.tpl', header=pizzas[0].keys(), rows=pizzas)
+
+# example page: https://developers.google.com/chart/interactive/docs/gallery/linechart
+
+@route('/line')
+def get_chart():
+    data = [          
+        ['2004',  1000,      400],
+        ['2005',  1170,      460],
+        ['2006',  660,       1120],
+        ['2007',  1030,      540],
+        ['2008',  1000,      400],
+        ['2009',  1170,      460],
+        ['2010',  660,       1120],
+        ['2011',  1030,      540],
+        ['2012',  1000,      400],
+        ['2013',  1170,      460],
+        ['2014',  660,       1120],
+        ['2015',  1030,      540]
+    ]
+    data = [ {'year':x, 'sales':y, 'expenses':z} for x,y,z in data ]
+    return template("line_chart.tpl", rows=data)
+
+@route('/weather')
+def get_chart():
+    #data = [ {'time':x, 'temp':y, 'humidity':z} for x,y,z in data ]
+    stream="beta"
+    #return template('<b>Hello {{name}}</b>!', name=name)
+    url = "http://drdelozier.pythonanywhere.com/stream/query/"
+    payload = {
+        'time' : 0,
+        'userid': 0,
+        'city': 0,
+        'state': 0,
+        'lat': 0,
+        'lon': 0,
+        'temp': 0,
+        'humidity': 0,
+        'light': 0,
+        'outdoors': 0,
+    }
+    response = requests.get(url + stream)
+    data = response.json()
+    data = data['result']
+    for key in payload.keys():
+        data = [item for item in data if key in item]
+    print(data)
+    data = [{'time':item['time'], 'temp':item['temp'], 'humidity':item['humidity']} for item in data]
+    return template("weather_chart.tpl", rows=data)
+    #return template('make_dict_table.tpl', header=['time','temp','humidity'], rows=data)
 
 @route('/')
 def get_index():
---
+++ code/webapp/weather_chart.tpl
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Time', 'Temperature', 'Humidity'],
+          %for row in rows:
+            [ {{row['time']}}, {{row['temp']}}, {{row['humidity']}} ],
+          %end
+        ]);
+
+        var options = {
+          title: 'Weather',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+  </body>
+</html>
